831f3ef83b23f35b6c6a6ab6d54d7af4fd41f549
Lemnoideae <aquan5584j@gmail.com>
2026-09-01 12:19:26 +0900

    12

--- back/src/main/java/com/back/p67260811/domain/member/entity/Member.java
+++ back/src/main/java/com/back/p67260811/domain/member/entity/Member.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @NoArgsConstructor
 @Getter
 @Entity
@@ -13,11 +15,13 @@ public class Member extends BaseEntity {
     private String username;
     private String password;
     private String nickname;
+    private String apiKey;
 
     public Member(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+        this.apiKey = UUID.randomUUID().toString();
     }
 
 }
