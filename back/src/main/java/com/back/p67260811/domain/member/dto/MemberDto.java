67ad507203c359b0b4058a063467b322843ef008
Lemnoideae <aquan5584j@gmail.com>
2026-09-02 09:19:01 +0900

    15

--- back/src/main/java/com/back/p67260811/domain/member/dto/MemberDto.java
+++ back/src/main/java/com/back/p67260811/domain/member/dto/MemberDto.java
@@ -1,16 +1,23 @@
 package com.back.p67260811.domain.member.dto;
 
+import com.back.p67260811.domain.member.entity.Member;
 import lombok.Builder;
+
+import java.time.LocalDateTime;
 
 @Builder
 public record MemberDto(
-        String nickname,
-        String username
+        int id,
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String name
 ) {
-    public static MemberDto from(String nickname, String username) {
+    public static MemberDto from(Member member) {
         return MemberDto.builder()
-                .nickname(nickname)
-                .username(username)
+                .id(member.getId())
+                .createDate(member.getCreateDate())
+                .modifyDate(member.getModifyDate())
+                .name(member.getUsername())
                 .build();
     }
 }
